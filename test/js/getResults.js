a2aca1a2f87caa2bb0e53313e2ab209566f6ddef
thomasvallet <thomasvallet@hotmail.fr>
2017-04-06 09:31:34 +0200

    Test page for new API

---
+++ test/js/getResults.js
@@ -0,0 +1,158 @@
+var snippetApiBaseUrl = "http://babylonjs-api2.azurewebsites.net/snippets/";
+var statusMessageGet = document.getElementById('statusMessageGet');
+var statusMessagePost = document.getElementById('statusMessagePost');
+// function onSaveNewSnippetClick() {
+//     var json = document.getElementById("nameSnippet").value;
+//     json += document.getElementById("descSnippet").value;
+//     json += document.getElementById("tagSnippet").value;
+//     json += document.getElementById("payloadSnippet").value;
+//     var jsonObject = JSON.parse(json);
+//     var existingId = document.getElementById("existingId").value;
+
+//     var url = snippetApiBaseUrl + "snippet";
+
+//     if (existingId != "") {
+//         url += "/" + existingId;
+//     }
+
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function () {
+//         if (xmlHttp.readyState == 4) {
+//             switch (xmlHttp.status) {
+//                 case 201:
+//                     var snippet = JSON.parse(xmlHttp.responseText);
+//                     var snippetUrl = url + "/" + snippet.id;
+//                     if (snippet.version != "0") {
+//                         snippetUrl += "/" + snippet.version;
+//                     }
+//                     document.getElementById("snippetUrl").value = snippetUrl;
+
+//                 case 204:
+//                     console.warn('No content');
+//                     break;
+//                 case 400:
+//                     console.error('Bad request');
+//                     break;
+//                 case 401:
+//                     console.error('Unauthorized');
+//                     break;
+//                 case 404:
+//                     console.error('Not found');
+//                     break;
+//                 case 500:
+//                     console.error('Internal server error');
+//                     break;
+//             }
+//         }
+//     }
+
+//     var jsonAsString = JSON.stringify(jsonObject);
+//     xmlHttp.open("POST", url, true);
+//     xmlHttp.setRequestHeader("Content-Type", "application/json");
+//     xmlHttp.send(jsonAsString);
+// }
+
+function onGetSnippetClick() {
+    var url = snippetApiBaseUrl+document.getElementById("snippetUrl").value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+            switch (xmlHttp.status) {
+                case 200:
+                    var jsonObject = JSON.parse(xmlHttp.responseText);
+                    alert(xmlHttp.responseText);
+                    break;
+                case 204:
+                statusMessageGet.innerText = 'No content';
+                    console.warn('No content');
+                    break;
+                case 400:
+                statusMessageGet.innerText = 'Bad request';
+                    console.error('Bad request');
+                    break;
+                case 401:
+                statusMessageGet.innerText = 'Unauthorized';
+                    console.error('Unauthorized');
+                    break;
+                case 404:
+                statusMessageGet.innerText = 'Not found';
+                    console.error('Not found');
+                    break;
+                case 500:
+                    console.error('Internal server error');
+                    break;
+            }
+        }
+    }
+
+    xmlHttp.open("GET", url, true);
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlHttp.send();
+}
+
+function onSearchSnippetClick(searchType) {
+    var url = snippetApiBaseUrl + "search";
+    switch (searchType) {
+        case 'snippetSearchDesc':
+            url += '/name';
+            break;
+        case 'snippetSearchTag':
+            url += '/tags';
+            break;
+        case 'snippetSearchCode':
+            url += '/code/';
+            break;
+    }
+    document.getElementById("searchResults").value = "";
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4) {
+            switch (xmlHttp.status) {
+                case 200:
+                    var searchResponse = JSON.parse(xmlHttp.responseText);
+                    for (var snippetIndex of searchResponse) {
+                        document.getElementById("searchResults").value += (snippetIndex.id + "\r\n");
+                    }
+                    break;
+                case 204:
+                statusMessagePost.innerText = 'No content';
+                    console.warn('No content');
+                    break;
+                case 400:
+                statusMessagePost.innerText = 'Bad request';
+                    console.error('Bad request');
+                    break;
+                case 401:
+                statusMessagePost.innerText = 'Unauthorized';
+                    console.error('Unauthorized');
+                    break;
+                case 404:
+                statusMessagePost.innerText = 'Not found';
+                    console.error('Not found');
+                    break;
+                case 500:
+                statusMessagePost.innerText = 'Internal server error';
+                    console.error('Internal server error');
+                    break;
+            }
+        }
+    }
+
+    xmlHttp.open("POST", url, true);
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    //Send the proper header information along with the request
+    xmlHttp.setRequestHeader("Content-type", "application/json");
+    xmlHttp.send(JSON.stringify({
+        "search": document.getElementById(searchType).value,
+        "page": 0,
+        "pageSize": 10,
+        "includePayload": false
+    }));
+}
+
+//document.getElementById("btnSaveSnippet").addEventListener("click", onSaveNewSnippetClick);
+document.getElementById("btnGetSnippet").addEventListener("click", onGetSnippetClick);
+document.getElementById("btnSearchSnippetDesc").addEventListener("click", function () { onSearchSnippetClick('snippetSearchDesc') }, true);
+document.getElementById("btnSearchSnippetTag").addEventListener("click", function () { onSearchSnippetClick('snippetSearchTag') }, true);
+document.getElementById("btnSearchSnippetCode").addEventListener("click", function () { onSearchSnippetClick('snippetSearchCode') }, true);
